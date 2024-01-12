e5e322f71cd973e6765fac4a317b4b4c18f90e0f
Hitesh918 <118730681+Hitesh918@users.noreply.github.com>
2024-01-12 16:47:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,115 +10,100 @@ const client = new MongoClient(uri, {});
 
 app.use(cors());
 
-
-
 let input
-let collections=["crime_scene_reports" , "persons"  , "incomes" , "interviews" , "licenses" , "get_fit_now_check_ins" , "get_fit_now_members" , "facebook_event_checkins"]
+let collections = ["crime_scene_reports", "people", "incomes", "interviews", "licenses", "get_fit_now_check_ins", "get_fit_now_members", "facebook_event_checkins"]
 
-let first , second , third , query , errMsg
+let first, second, third, query
+let errMsg = "no"
 
-function queryProcessor(str){
-    first=str.slice(0 , 3)
-    if(first != "db."){
-      errMsg = "wrong query 1"
-      console.error("wrong query 1")
-    }
-    else{
-        let x=str.indexOf("." , 3)
-        second=str.slice(3,x)
-        if(collections.includes(second)){
-            if(str[x]=="." ){
-                let y= str.indexOf("(" , x+1)
-                third=str.slice(x+1 , y)
-                let l=str.length
-                if(str[l-1]==")"){
-                    query=str.slice(y+1 , l-1)
-                    console.log(second , third , query)
-                    // query=query.replace(/(['"])?([a-z0-9A-Z_]+)(['"])?:/g, '"$2": ')
-                    // query = query.replace(/([{,]\s*)([a-zA-Z_$][\w$]*)(\s*:)/g, '$1"$2"$3');
-                    query = query.replace(/(\$?\b\w+\b)(?=\s*:)/g, '"$1"');
-
-
-
-                    console.log(second , third , query)
-
-                }
-                else{
-                  errMsg = "wrong query 4"
-
-                    console.error("wrong error 4")
-                }
-            }
-            else{
-              errMsg = "wrong query 3"
-
-                console.error("wrong query 3")
-            }
-        }
-        else{
-            console.error("wrong query 2")
-            errMsg = "wrong query 2"
-
-        }
-    }
+function queryProcessor(str) {
+  first = str.slice(0, 3)
+  if (first != "db.") {
+    errMsg = "The query must start with db. "
+    console.error("The query must start with db. ")
   }
+  else {
+    let x = str.indexOf(".", 3)
+    second = str.slice(3, x)
+    if (collections.includes(second)) {
+      if (str[x] == ".") {
+        let y = str.indexOf("(", x + 1)
+        third = str.slice(x + 1, y)
+        let l = str.length
+        if (str[l - 1] == ")") {
+          query = str.slice(y + 1, l - 1)
+          console.log(second, third, query)
+          query = query.replace(/(\$?\b\w+\b)(?=\s*:)/g, '"$1"');
+          console.log(second, third, query)
+        }
+        else {
+          errMsg = "Missing brackett )"
+          console.error("Missing brackett )")
+        }
+      }
+      else {
+        errMsg = `Syntax Error . Missing dot(.)`
 
-async function run() {
-    queryProcessor(input)
-  try {
-    await client.connect();
-    console.log('Connected to the database');
-
-    const database = client.db('crimeDB');
-    const collection = database.collection(second); 
-    let documents
-
-    if(third=="find"){
-        documents = await collection.find(JSON.parse(query)).toArray();
-        // console.log('Documents in the collection:', documents);
-        return documents
+        console.error(`Syntax Error . Missing dot(.)`)
+      }
     }
-    else if(third=="findOne"){
-        documents = await collection.findOne(JSON.parse(query)).toArray();
-        // console.log('Documents in the collection:', documents);
-        return documents
+    else {
+      console.error("Collection name is invalid")
+      errMsg = "Collection name is invalid"
     }
-    else if(third=="aggregate"){
-        documents = await collection.aggregate(JSON.parse(query)).toArray();
-        // console.log('Documents in the collection:', documents);
-        return documents
-    }
-    else{
-        console.error("cannot be executed")
-        return errMsg
-    }
-  }
-  finally {
-    await client.close();
   }
 }
 
-// run().catch(console.dir);
+async function run() {
+  queryProcessor(input)
+  if (errMsg == "no") {
+    try {
+      await client.connect();
+      console.log('Connected to the database');
 
+      const database = client.db('crimeDB');
+      const collection = database.collection(second);
+      let documents
+      if (third == "find") {
+        documents = await collection.find(JSON.parse(query)).toArray();
+        return documents
+      }
+      else if (third == "findOne") {
+        documents = await collection.findOne(JSON.parse(query)).toArray();
+        return documents
+      }
+      else if (third == "aggregate") {
+        documents = await collection.aggregate(JSON.parse(query)).toArray();
+        return documents
+      }
+      else {
+        console.error("cannot be executed")
+        return [{ "Error": "You only have read access to database" }]
+      }
+    }
+    finally {
+      await client.close();
+    }
+  }
+  else {
+    return [{ "Error": errMsg }]
+  }
+}
 
 app.get('/', async (req, res) => {
-  input = req.query.input
-  // input = "db.interviews.find({ person_id: 14887 })"
-  // input = `db.licenses.aggregate([ { $match: { gender: "male" } } ])`
+  input = req.query.input.trim()
   console.log(input)
-    try {
-      const resp = await run();
-      console.log(resp)
-      res.send(resp);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('An error occurred');
-    }
-  });
-  
-  // exports.app = functions.https.onRequest(app);
+  try {
+    const resp = await run();
+    console.log(resp)
+    errMsg="no"
+    res.send(resp);
+  } catch (error) {
+    console.error(error);
+    res.send([{ "Error": "Syntax Mistake , Please check your query" }])
 
+  }
+});
 
-  // app.listen(5000)
 const PORT=process.env.PORT||5000
-  app.listen(PORT)
+app.listen(PORT)
